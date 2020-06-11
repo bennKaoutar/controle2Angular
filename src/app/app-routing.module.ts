efc08bed01d829421d69a857ae258069e4360b11
bennKaoutar <49061347+bennKaoutar@users.noreply.github.com>
2020-06-11 17:00:33 +0100

    last fetch(Compiled successfully)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [{
   path: '', loadChildren: () => import('./cars/cars.module').then(m => m.CarsModule) },
    {path: 'cars', loadChildren: () => import('./cars/cars.module').then(m => m.CarsModule) },
-  { path: 'CarDetails', loadChildren: () => import('./car-details/car-details.module').then(m => m.CarDetailsModule) },
+  { path: 'CarDetails/:car', loadChildren: () => import('./car-details/car-details.module').then(m => m.CarDetailsModule) },
   { path: 'erreur', loadChildren: () => import('./erreur/erreur.module').then(m => m.ErreurModule) },
   { path: '**', redirectTo: "erreur" }
 ];
