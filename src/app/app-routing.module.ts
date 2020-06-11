1ebc5d30ce389e8e53d00ddbada050fb633e6f6e
bennKaoutar <49061347+bennKaoutar@users.noreply.github.com>
2020-06-11 14:21:29 +0100

    add components
    car and car details also error page

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [{
+  path: '', loadChildren: () => import('./cars/cars.module').then(m => m.CarsModule) },
+   {path: 'Cars', loadChildren: () => import('./cars/cars.module').then(m => m.CarsModule) },
+  { path: 'CarDetails', loadChildren: () => import('./car-details/car-details.module').then(m => m.CarDetailsModule) },
+  { path: 'erreur', loadChildren: () => import('./erreur/erreur.module').then(m => m.ErreurModule) },
+  { path: '**', redirectTo: "erreur" }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
